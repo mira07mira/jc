436526bd2fcf934723f43daf8c21fd430c212275
mira <mira@gmail.com>
2023-08-20 16:13:37 +0300

    last

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+var name = prompt('Введите своё имя')
+var year = +prompt('Введите свой возраст')
+var namber1= +prompt('346 + 123 = ?')
+var namber2 = +prompt('59 - 137 = ?')
+var namber3 = +prompt('531 * 25 = ?')
+var namber4 = +prompt('297 / 99 = ?')
+var namber5 = +prompt('694 % 2 = ?')
+var str = prompt('Откройте консоль для просмотра:' + ' Ctrl+Shift+j')
+
+
+
+
+
+
+console.log('Приветствую тебя, друг мой)');
+console.log('Меня зовут ' + name);
+console.log('Мне ' + year + ' лет ');
+console.log('Первый пример:' + ' 456 + 123 = 468 ' + ' Ваш ответ: ' + namber1);
+console.log('Второй пример:' + ' 59 - 137 = -78 ' + ' Ваш ответ: ' + namber2);
+console.log('Третий пример:' + ' 531 * 25 = 13 275 ' + ' Ваш ответ: ' + namber3);
+console.log('Четвертый пример:' + ' 297 / 99 = 3 ' + ' Ваш ответ: ' + namber4);
+console.log('Пятый пример:' + ' 694 / 9 = 77.1111111 ' + ' Ваш ответ: ' + namber4);
+
